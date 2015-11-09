acf33572e6553e473642e691d6d673c677e5beb1
Shinta Smith <shinta.smith@RACKSPACE.COM>
2015-11-09 11:28:22 -0600

    CF-1299: remove accidental debug option

--- src/main/scripts/buildDocs.sh
+++ src/main/scripts/buildDocs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 export MAVEN_OPTS="-Xms1024m -XX:MaxPermSize=1024m -Xmx1024m -Xrs"
-mvn clean -Dgenerate.docs process-sources -Dwebhelp.war=1 -X
+mvn clean -Dgenerate.docs process-sources -Dwebhelp.war=1 
